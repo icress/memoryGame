565e610b0de06e98835939d88a3bed218bfbda5c
icres <icress26@gmail.com>
2023-03-28 12:17:16 -0600

    Makes 3x4 grid

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let random = Math.floor(Math.random() * 16);
 let positions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
-const flip = function() {
+const flip = function () {
     if (this.getAttribute('src') != 'images/blank.png') {
         this.setAttribute('src', 'images/blank.png')
     } else {
         let name = this.getAttribute('name')
         this.setAttribute('src', `images/${name}.png`)
     }
-}
+};
 
 const itemArr = [
     {
@@ -35,7 +35,7 @@ const itemArr = [
         name: 'pizza',
         image: 'images/pizza.png'
     }
-]
+];
 
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
@@ -43,22 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const number = document.createElement('p');
     number.innerText = random;
     grid.appendChild(number);
-    const tileArr = document.querySelectorAll('.tile');
-    const rows = document.querySelectorAll('.row')
-
-    for (tile of tileArr) {
-        tile.setAttribute('name', itemArr[Math.floor(Math.random() * itemArr.length)].name)
-        tile.setAttribute('src', itemArr[Math.floor(Math.random() * itemArr.length)].image)
-        tile.addEventListener('click', () => {
-            if (tile.getAttribute('src') != 'images/blank.png') {
-                tile.setAttribute('src', 'images/blank.png')
-            } else {
-                let name = tile.getAttribute('name')
-                tile.setAttribute('src', `images/${name}.png`)
-            }
-        })
-    }
-
+    let rows = Array.from(document.querySelectorAll('.row'))
 
     for (item of itemArr) {
         for (let i = 0; i < 2; i++) {
@@ -66,7 +51,11 @@ document.addEventListener('DOMContentLoaded', () => {
             tile.setAttribute('src', 'images/blank.png');
             tile.setAttribute('name', item.name)
             tile.classList.add('tile');
-            grid.appendChild(tile);
+            let randRow = rows[Math.floor(Math.random() * rows.length)]
+            randRow.appendChild(tile);
+            if (randRow.children.length >= 4) {
+                rows.splice(rows.indexOf(randRow), 1)
+            }
             tile.addEventListener('click', () => {
                 if (tile.getAttribute('src') != 'images/blank.png') {
                     tile.setAttribute('src', 'images/blank.png')
